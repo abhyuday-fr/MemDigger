daaecd936122ba8272acf5b646c5d1cb43431f92
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-24 23:35:02 +0530

    added memory bandwidth benchmark logic with sequential read loop

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include <algorithm>
 #include <chrono>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <numeric>
@@ -21,8 +22,9 @@ struct Node {
 };
 
 // run_latency_benchmark runs the benchmark and puts data in a csv file.
-// test sizes from 4KB (L1 Cache) up to 64MB (main memory)
 void run_latency_benchmark(std::ofstream &csv_file) {
+
+  // test sizes from 4KB (L1 Cache) up to 64MB (main memory)
   std::vector<size_t> sizes_kb = {4, 16, 64, 256, 1024, 4096, 16384, 65536};
 
   for (size_t size_kb : sizes_kb) {
@@ -66,6 +68,33 @@ void run_latency_benchmark(std::ofstream &csv_file) {
 
     csv_file << "Latency," << size_kb << "," << latency_per_access << "\n";
   }
+}
+
+void run_bandwidth_benchmark(std::ofstream &csv_file) {
+
+  // 256 mb buffer forces reads from main memory, bypassing cache limits
+  size_t size_bytes = 256 * 1024 * 1024;
+  std::vector<uint64_t> buffer(size_bytes / sizeof(uint64_t), 1);
+
+  uint64_t sum = 0;
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  // sequential read loop
+  for (size_t i = 0; i < buffer.size(); i++) {
+    sum += buffer[i];
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> time_s = end - start;
+
+  // convert to gb/s
+  double bandwidth_gbps =
+      (size_bytes / (1024.0 * 1024.0 * 1024.0)) / time_s.count();
+  csv_file << "Bandwidth,256MB," << bandwidth_gbps << "\n";
+
+  // log sum to volatile sink to prevent compiler optimization
+  volatile uint64_t sink = sum;
 }
 
 int main() {
